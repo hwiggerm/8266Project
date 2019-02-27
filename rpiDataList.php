9c2b6846e89b5f3f31051af1f83d60010c4f394a
hwiggerm <hwiggerm@gmail.com>
2019-02-27 18:49:07 +0100

    Update rpiDataList.php

--- rpiDataList.php
+++ rpiDataList.php
@@ -1,21 +1,16 @@
-<!DOCTYPE html>
 <?php
-
-
 //insert into temp_log(device,temp,humidity) VALUES ($device, $temp, $humidity)";
-
-
-
 $con=mysqli_connect("localhost","testuser","test623","testdb");
+
 // Check connection
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
-$result = mysqli_query($con,"select * from  temp_log  order by device desc ");
+$result = mysqli_query($con,"select * from temp_log order by tstamp");
 while($row = mysqli_fetch_array($result))
   {
-  echo $row['device']." ; " . $row['temp'];" . $row['humidity'];
+  echo $row['device']. ";" . $row['temp'] . ";" . $row['humidity'];
   echo "<br>";
   }
 mysqli_close($con);
